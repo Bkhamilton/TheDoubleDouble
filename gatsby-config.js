3b4cadcc59fb011f94250ad5d1300194f85af4cc
Bkhamilton <bkhamilton310@gmail.com>
2023-06-21 23:19:35 -0400

    Small Changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,9 +14,9 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `The Double Double`,
+    description: `Welcome to the website for The Double Double, an NBA Podcast hosted by Ben Hamilton and J.P. Carey.`,
+    author: `@bkhamilton`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
