822dcb079004169485e4dd60127eede2e6c540bb
Bkhamilton <bkhamilton310@gmail.com>
2024-09-27 17:00:25 -0400

    updated TestEpisode for recent upload

--- src/components/TheDoubleDouble/TestEpisode/TestEpisode.jsx
+++ src/components/TheDoubleDouble/TestEpisode/TestEpisode.jsx
@@ -10,14 +10,14 @@ export default function TestEpisode() {
         <div className='episode-node-outer-container'>
             <div className='episode-node-container'>
                 <div className='episode-info-container'>
-                    <span className='episode-info-text'>June 24 | Season 3 | 1:02:38</span>
-                    <h1 className='shadow-text'>2023 NBA Draft Review</h1>
-                    <span className='episode-description'>The NBA Draft is one of our favorite parts of the NBA Season. We've spent a lot of time over the past researching these prospects and building our own mock drafts, and last night was the night of the draft. In this episode...</span>
+                    <span className='episode-info-text'>Sep 26 | Season 5 | 1:29:00</span>
+                    <h1 className='shadow-text'>2024-2025 Eastern Conference Breakdown</h1>
+                    <span className='episode-description'>This year's Eastern Conference Breakdown is here! We like to do one of these every season as weget close to the start of the season, and we start with the East. This episode,...</span>
                     <div className='listen-button-container'>
-                        <a href={`https://open.spotify.com/episode/5XCMWBRbO9ehszSHu7Cr1V?si=f83e825351394db7`} target="_blank" rel="noopener noreferrer" className='spotify-button'>
+                        <a href={`https://open.spotify.com/episode/4Hdz8bGfVhAN5jCmxUc5Jg`} target="_blank" rel="noopener noreferrer" className='spotify-button'>
                             <img src={spotifyIcon} alt="Spotify Listen Button"></img>
                         </a>
-                        <a href='https://podcasts.apple.com/us/podcast/2023-nba-draft-review/id1555765326?i=1000618155716' target="_blank" rel="noopener noreferrer" className='apple-podcast-button'>
+                        <a href='https://podcasts.apple.com/us/podcast/2024-2025-eastern-conference-breakdown/id1555765326?i=1000670870405' target="_blank" rel="noopener noreferrer" className='apple-podcast-button'>
                             <img src={appleIcon} alt="Apple Podcast Listen Button"></img>
                         </a>
                     </div>
